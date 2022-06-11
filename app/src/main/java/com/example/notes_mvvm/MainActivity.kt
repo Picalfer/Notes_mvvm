379799590b6edca467753cb4507dd5a14d8b0fff
picalfer <trable228@icloud.com>
2022-06-11 13:00:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notes_mvvm/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.notes_mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
