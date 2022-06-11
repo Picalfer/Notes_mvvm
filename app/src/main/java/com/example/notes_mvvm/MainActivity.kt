42150fb92c684ed0e09c2c9d904382072f884b1c
picalfer <trable228@icloud.com>
2022-06-11 13:15:14 +0300

    set toolbar, init objects in main activity

--- app/src/main/java/com/example/notes_mvvm/MainActivity.kt
+++ app/src/main/java/com/example/notes_mvvm/MainActivity.kt
@@ -2,13 +2,33 @@ package com.example.notes_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.notes_mvvm.databinding.ActivityMainBinding
+import com.example.notes_mvvm.utilits.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
 
-    
+    lateinit var mToolbar: Toolbar
+    lateinit var mNavController: NavController
+    private var _binding: ActivityMainBinding? = null
+    val mBinding get() = _binding!! // переменная для использования байдинга где не будет проверки,
+    // что он null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        APP_ACTIVITY = this
+        mToolbar = mBinding.toolbar
+        mNavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setSupportActionBar(mToolbar)
+        title = getString(R.string.title)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/example/notes_mvvm/utilits/constants.kt
@@ -0,0 +1,5 @@
+package com.example.notes_mvvm.utilits
+
+import com.example.notes_mvvm.MainActivity
+
+lateinit var APP_ACTIVITY: MainActivity
